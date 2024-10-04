189c39d7de239693d07521fc74b59fcad6400ef9
ignasijuez <ignasi.juezguirao@student.kuleuven.be>
2024-10-04 11:42:05 +0200

    test for 01 script

--- src/main/resources/db/mysql_k8s/01_user.sql
+++ src/main/resources/db/mysql_k8s/01_user.sql
@@ -4,4 +4,8 @@ ALTER DATABASE petclinic
   DEFAULT CHARACTER SET utf8
   DEFAULT COLLATE utf8_general_ci;
 
-GRANT ALL PRIVILEGES ON petclinic.* TO 'petclinic'@'%' IDENTIFIED BY 'petclinic';
+-- First, create the user
+CREATE USER IF NOT EXISTS 'petclinic'@'%' IDENTIFIED BY 'petclinic';
+
+-- Then, grant privileges to the user
+GRANT ALL PRIVILEGES ON petclinic.* TO 'petclinic'@'%';
