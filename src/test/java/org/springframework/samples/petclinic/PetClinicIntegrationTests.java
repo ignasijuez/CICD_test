7ade33b04fe86579d13bf384f61d2d915d7310d1
ignasijuez <ignasi.juezguirao@student.kuleuven.be>
2024-10-07 12:59:15 +0200

    a

--- src/test/java/org/springframework/samples/petclinic/PetClinicIntegrationTests.java
+++ src/test/java/org/springframework/samples/petclinic/PetClinicIntegrationTests.java
@@ -31,8 +31,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.samples.petclinic.vet.VetRepository;
 import org.springframework.web.client.RestTemplate;
 
-//@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
-@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT, properties = {"spring.profiles.active=mysql"})
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+//@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT, properties = {"spring.profiles.active=mysql"})
 
 public class PetClinicIntegrationTests {
 
